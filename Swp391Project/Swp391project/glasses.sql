937b4a2d72ad4d3f9c5fbba87c0e4cccf8673fdc
anhhqhe160800 <anhhqhe160800@fpt.edu.vn>
2023-01-28 00:06:42 +0700

    Create glasses.sql

---
+++ Swp391Project/Swp391project/glasses.sql
@@ -0,0 +1,39 @@
+INSERT INTO `swp`.`glasses`
+(`glassesID`,`glassesName`,`color`,`gender`,`material`,`style`,`image`,`price`)
+VALUES
+('glasses1','TITANIUM LENSES','Yellow',1,'Titan','Rectangular eyes','https://kinhmateyeplus.com/wp-content/uploads/2023/01/IMG_0028.jpg','944.000');
+
+INSERT INTO `swp`.`glasses`
+(`glassesID`,`glassesName`,`color`,`gender`,`material`,`style`,`image`,`price`)
+VALUES
+('glasses2','TITANIUM LENSES C1','Black',0,'Titan','Rectangular eyes','https://kinhmateyeplus.com/wp-content/uploads/2023/01/IMG_0023.jpg','944.000');
+
+INSERT INTO `swp`.`glasses`
+(`glassesID`,`glassesName`,`color`,`gender`,`material`,`style`,`image`,`price`)
+VALUES
+('glasses3','TITANIUM LENSES C3','While',1,'Titan','Rectangular eyes','https://kinhmateyeplus.com/wp-content/uploads/2023/01/IMG_0035.jpg','944.000');
+
+INSERT INTO `swp`.`glasses`
+(`glassesID`,`glassesName`,`color`,`gender`,`material`,`style`,`image`,`price`)
+VALUES
+('glasses4','TITANIUM LENSES C4','While',1,'Titan','Rectangular eyes','https://kinhmateyeplus.com/wp-content/uploads/2023/01/IMG_0016.jpg','944.000');
+
+INSERT INTO `swp`.`glasses`
+(`glassesID`,`glassesName`,`color`,`gender`,`material`,`style`,`image`,`price`)
+VALUES
+('glasses5','TITANIUM LENSES 101','While',0,'Titan','Rectangular eyes','https://kinhmateyeplus.com/wp-content/uploads/2023/01/IMG_9995.jpg','944.000');
+
+INSERT INTO `swp`.`glasses`
+(`glassesID`,`glassesName`,`color`,`gender`,`material`,`style`,`image`,`price`)
+VALUES
+('glasses6','TITANIUM LENSES C24','While',1,'Titan','Rectangular eyes','https://kinhmateyeplus.com/wp-content/uploads/2023/01/IMG_9984.jpg','944.000');
+
+INSERT INTO `swp`.`glasses`
+(`glassesID`,`glassesName`,`color`,`gender`,`material`,`style`,`image`,`price`)
+VALUES
+('glasses7','TITANIUM LENSES C48','While',1,'Titan','Rectangular eyes','https://kinhmateyeplus.com/wp-content/uploads/2023/01/IMG_9976.jpg','944.000');
+
+INSERT INTO `swp`.`glasses`
+(`glassesID`,`glassesName`,`color`,`gender`,`material`,`style`,`image`,`price`)
+VALUES
+('glasses8','TITANIUM LENSES Limited','While',0,'Titan','Rectangular eyes','https://kinhmateyeplus.com/wp-content/uploads/2023/01/IMG_0089.jpg','944.000');
