6c04d9dc30b628c94d2e9d45b99154a307e0ceea
vubahe163495 <vubahe163495@fpt.edu.vn>
2023-01-30 11:38:48 +0700

    update

---
+++ Swp391Project/Swp391project/src/main/java/com/groud2/web/model/profile.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.groud2.web.model;
+
+/**
+ *
+ * @author asus
+ */
+public class profile {
+        private String UserId;
+        private String name;
+        private String surname;
+        private String phoneNumber;
+        private String address;
+        private String postcode;
+        private String state;
+        private String area;
+        private String emailID;
+      
+        
+}
